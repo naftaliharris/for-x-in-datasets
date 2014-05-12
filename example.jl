804fd009263e0c8a866b85a61e18e6857e477306
Naftali Harris <naftaliharris@gmail.com>
2014-05-12 15:23:33 -0700

    Using julia DataFrame instead of readcsv

--- example.jl
+++ example.jl
@@ -1,6 +1,7 @@
 # A Julia example of how to work with these datasets.
 
-using JSON  # if you don't have this, install it with julia> Pkg.add("JSON")
+using JSON        # if you don't have this, install it with julia> Pkg.add("JSON")
+using DataFrames  # if you don't have this, install it with julia> Pkg.add("DataFrames")
 
 for ds in readdir("datasets")
     md_file = joinpath("datasets", ds, "METADATA.json")
@@ -14,8 +15,8 @@ for ds in readdir("datasets")
     #if y_type == "numeric" && p < n
     #if y_type in ["binary", "categorical"] && !X_missing
     if true
-        X = readcsv(joinpath("datasets", ds, "X.csv"))
-        y = readcsv(joinpath("datasets", ds, "y.csv"))
+        X = readtable(joinpath("datasets", ds, "X.csv"))
+        y = readtable(joinpath("datasets", ds, "y.csv"))
 
         # Do some stats or ML with X and y here
     end
