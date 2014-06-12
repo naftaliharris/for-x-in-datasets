d1055dd6eee50bc631fc9c113ca73a3299dd3e80
Naftali Harris <naftaliharris@gmail.com>
2014-06-11 18:06:39 -0700

    Fixed example.jl; test.py passes again!

--- example.jl
+++ example.jl
@@ -1,23 +1,27 @@
 # A Julia example of how to work with these datasets.
 
-using JSON        # if you don't have this, install it with julia> Pkg.add("JSON")
-using DataFrames  # if you don't have this, install it with julia> Pkg.add("DataFrames")
+using DataFrames  # if you don't have this, install it with:
+                  # julia> Pkg.add("DataFrames")
 
 for ds in readdir("datasets")
-    md_file = joinpath("datasets", ds, "METADATA.json")
-    md = JSON.parse(readall(md_file))
+    md_file = joinpath("datasets", ds, "METADATA.tsv")
+    md = readtable(md_file)
 
-    n, p = md["X"]["rows"], md["X"]["cols"]
-    y_type, X_types = md["y"]["type"], md["X"]["types"]
-    X_missing = md["X"]["missing"]
+    n, p = md[:n][1], md[:p][1]
+    y_type = md[:y_type][1]
+    X_numeric = md[:numeric_X][1]
+    X_binary = md[:binary_X][1]
+    X_categorical = md[:categorical_X][1]
+    X_missing = md[:Missing][1]
 
     # Determine whether to use the dataset. Example rules:
     #if y_type == "binary" && n > 1000
     #if y_type == "numeric" && p < n
     #if y_type in ["binary", "categorical"] && !X_missing
     if true
-        X = readtable(joinpath("datasets", ds, "X.csv"))
-        y = readtable(joinpath("datasets", ds, "y.csv"))
+        data = readtable(joinpath("datasets", ds, "DATA.tsv"))
+        X = data[2:end]
+        y = data[1]
 
         # Do some stats or ML with X and y here
     end
