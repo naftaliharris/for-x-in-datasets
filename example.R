13a86b7b36636d4b3b7155dd73d95100cc207cb9
Naftali Harris <naftaliharris@gmail.com>
2014-06-11 17:55:08 -0700

    Fixed R script for new spec

--- example.R
+++ example.R
@@ -1,22 +1,23 @@
 # An R example of how to work with these datasets.
 
-library(rjson)  # you'll need to use install.package("rjson") if you don't have this
-
 for (ds in list.files("datasets")) {
-    md <- fromJSON(file=file.path("datasets", ds, "METADATA.json"))
-    n <- md$X$rows
-    p <- md$X$cols
-    y_type <- md$y$type
-    X_types <- md$X$types
-    X_missing <- md$X$missing
+    md <- read.delim(file.path("datasets", ds, "METADATA.tsv"))
+    n <- md$n[0]
+    p <- md$p[0]
+    y_type <- md$y.type[0]
+    X_numeric = md$numeric.X.[0]
+    X_binary = md$binary.X.[0]
+    X_categorical = md$categorical.X.[0]
+    X_missing <- md$Missing.
 
     # Determine whether to use the dataset. Example rules:
     #if (y_type == "binary" && n > 1000) {
     #if ((y_type == "numeric" && p < n) {
     #if ((y_type %in% c("binary", "categorical") && !X_missing) {
     if (TRUE) {
-        X <- read.csv(file.path("datasets", ds, "X.csv"))
-        y <- read.csv(file.path("datasets", ds, "y.csv"))[,1]
+        data <- read.delim(file.path("datasets", ds, "DATA.tsv"))
+        X <- data[,-1]
+        y <- data[,1]
 
         # Do some stats or ML with X and y here
     }
